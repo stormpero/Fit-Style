4ae2cb50d975f2ccf003f05c0e30605a7d7fb146
FStrean <fustone.steamer@mail.ru>
2021-10-27 03:48:05 +0300

    Design Profile
    feature: Add design profile
    
    JIRA: FITS-26

--- src/root/frontend/src/pages/profile/Profile.jsx
+++ src/root/frontend/src/pages/profile/Profile.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import "./Profile.css";
+
 import LStorageUser from "../../services/LStorageUser";
+import ProfilePicture from "../../assets/default-profile-picture.jpg";
 
 export default class Profile extends Component {
   state = {
@@ -16,34 +19,63 @@ export default class Profile extends Component {
     const { currentUser } = this.state;
 
     return (
-      <div className="container">
+
+      <div className="container profile-info">
         {
           (this.state.userReady) ?
-          <div>
-          <header className="jumbotron">
-            <h3>
-              <strong>{currentUser.username}</strong> Profile
-            </h3>
-          </header>
-          <p>
-            <strong>Token:</strong>{" "}
-            {currentUser.accessToken.substring(0, 20)} ...{" "}
-            {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-          </p>
-          <p>
-            <strong>Id:</strong>{" "}
-            {currentUser.id}
-          </p>
-          <p>
-            <strong>Email:</strong>{" "}
-            {currentUser.email}
-          </p>
-          <strong>Authorities:</strong>
-          <ul>
-            {currentUser.roles &&
-              currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-          </ul>
-        </div>: null
+          <div className="d-flex justify-content-between">
+            <div className="left-image">
+              <img className="picture-profile" src={ProfilePicture} alt="Fit-Style"/>
+            </div>
+            <div className="right-info d-flex justify-content-between">
+              <div className="first-column">
+              <p>
+                <label> ID </label>
+                <strong>{currentUser.id}</strong>
+              </p>
+              <p>
+                <label> ФИО </label>
+                <strong>{currentUser.name} </strong>
+                <strong>{currentUser.surname} </strong>
+                <strong>{currentUser.patronymic} </strong>
+              </p>
+              <p>
+                <label> Возраст </label>
+                <strong>{currentUser.age}</strong>
+              </p>
+              <p>
+                <label> Пол </label>
+                <strong>{currentUser.gender}</strong>
+              </p>
+              <p>
+                <label> Дата рождения </label>
+                <strong>{currentUser.birthdate}</strong>
+              </p>
+              </div>
+              <div className="second-column">
+              <p>
+                <label> Телефон </label>
+                <strong>{currentUser.telephone}</strong>
+              </p>
+              <p>
+                <label> Паспорт </label>
+                <strong>{currentUser.passport}</strong>
+              </p>
+              <p>
+                <label> Адрес </label>
+                <strong>{currentUser.address}</strong>
+              </p>
+              <p>
+                <label> Вид абонемента </label>
+                <strong>{currentUser.subscription}</strong>
+              </p>
+              <p>
+                <label> Дата окончания </label>
+                <strong>{currentUser.endDate}</strong>
+              </p>
+              </div>
+            </div>
+          </div>: null
         }
       </div>
     );
