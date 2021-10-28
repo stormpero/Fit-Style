fd06160bd52347098827dea54f3805e355a89734
stormpero <staralex.const@gmail.com>
2021-10-28 15:44:08 +0300

    add PermissionController & update ProfileController

---
+++ src/root/backend/src/main/java/ru/project/fitstyle/controllers/PermissionController.java
@@ -0,0 +1,37 @@
+package ru.project.fitstyle.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.project.fitstyle.models.User;
+import ru.project.fitstyle.payload.response.MessageResponse;
+import ru.project.fitstyle.repository.UserRepository;
+
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/perm")
+public class PermissionController {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public PermissionController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<?> getUserRoles(@RequestParam("id") Long id)
+    {
+        Optional<User> user = userRepository.findById(id);
+        User returnUser = user.orElse(null);
+        if(returnUser==null)
+        {
+            return ResponseEntity.badRequest().
+                    body(new MessageResponse("Error: user with that id doesn't exist!"));
+        }
+        return ResponseEntity.ok(returnUser.getRoles());
+    }
+}
--- src/root/backend/src/main/java/ru/project/fitstyle/controllers/ProfileController.java
+++ src/root/backend/src/main/java/ru/project/fitstyle/controllers/ProfileController.java
@@ -2,6 +2,7 @@ package ru.project.fitstyle.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.project.fitstyle.models.User;
 import ru.project.fitstyle.payload.response.MessageResponse;
@@ -12,20 +13,19 @@ import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/profile")
+@PreAuthorize("hasRole('USER')")
 public class ProfileController {
 
     UserRepository userRepository;
 
     @Autowired
-    public ProfileController (UserRepository userRepository)
-    {
+    public ProfileController (UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/profile")
-    public ResponseEntity<?> getUserProfileInfo(@RequestParam("id") Long id)
-    {
+    @GetMapping()
+    public ResponseEntity<?> getUserProfileInfo(@RequestParam("id") Long id) {
         Optional<User> user = userRepository.findById(id);
         User returnUser = user.orElse(
                 null
@@ -47,18 +47,5 @@ public class ProfileController {
                         returnUser.getPassport(),
                         returnUser.getAddress())
         );
-    }
-
-    @GetMapping("/roles")
-    public ResponseEntity<?> getUserRoles(@RequestParam("id") Long id)
-    {
-        Optional<User> user = userRepository.findById(id);
-        User returnUser = user.orElse(null);
-        if(returnUser==null)
-        {
-            return ResponseEntity.badRequest().
-                    body(new MessageResponse("Error: user with that id doesn't exist!"));
-        }
-        return ResponseEntity.ok(returnUser.getRoles());
     }
 }
