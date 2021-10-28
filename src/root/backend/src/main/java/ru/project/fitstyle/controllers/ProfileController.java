0b0de3e8e5e4998ae03c4ae6c98516e942cecb52
FStrean <fustone.steamer@mail.ru>
2021-10-28 15:21:30 +0300

    Made refactoring
    deleted (request/auth) : UserRequest
    updated (ProfileController) : imports

--- src/root/backend/src/main/java/ru/project/fitstyle/controllers/ProfileController.java
+++ src/root/backend/src/main/java/ru/project/fitstyle/controllers/ProfileController.java
@@ -2,16 +2,12 @@ package ru.project.fitstyle.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.project.fitstyle.models.User;
-import ru.project.fitstyle.payload.request.profile.UserRequest;
 import ru.project.fitstyle.payload.response.MessageResponse;
 import ru.project.fitstyle.payload.response.UserProfileResponse;
 import ru.project.fitstyle.repository.UserRepository;
 
-import javax.validation.Valid;
 import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
