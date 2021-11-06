e0e2485fad9bd0b2568057ad21a7b742a719b339
stormpero <staralex.const@gmail.com>
2021-11-06 17:54:14 +0300

    added refresh token support on frontend
    feature: RefreshToken frontend
    refactor: delete dfunction LStorageUser
    
    JIRA: FITS-44

--- src/root/frontend/src/services/jwt/JwtService.js
+++ src/root/frontend/src/services/jwt/JwtService.js
@@ -10,11 +10,19 @@ class JwtService {
     };
 
     getAccessToken() {
-        return LStorageUser.getAccessToken();
+        const user = LStorageUser.getUser();
+        return user ? user?.accessToken : null;
     }
 
     getRefreshToken() {
+        const user = LStorageUser.getUser();
+        return user ? user?.refreshToken : null;
+    }
 
+    updateAccessToken(accessToken) {
+        const user = LStorageUser.getUser();
+        user.accessToken = accessToken;
+        LStorageUser.setUser(user);
     }
 }
 
