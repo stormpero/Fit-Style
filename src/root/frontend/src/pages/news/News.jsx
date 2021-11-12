a86f7f1f39fd075e103030876b98c97645533b30
stormpero <staralex.const@gmail.com>
2021-11-12 22:44:01 +0300

    refactor news

--- src/root/frontend/src/pages/news/News.jsx
+++ src/root/frontend/src/pages/news/News.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./News.css";
 
 import Gym from "../../assets/gym.jpg";
--- src/root/frontend/src/pages/news/NewsBord.jsx
+++ src/root/frontend/src/pages/news/NewsBord.jsx
@@ -2,19 +2,6 @@ import React, { Component } from "react";
 import NewsRow from "./NewsRow";
 import UserService from "../../services/UserService";
 import DateFormat from "../../services/utils/DateFormat";
-import News from "./News";
-
-
-function sliceArray(array, size){ // return arrays of roles, arrays length <= size
-    return array.reduce((p,c)=>{
-        if(p[p.length-1].length === size){
-            p.push([]);
-        }
-
-        p[p.length-1].push(c);
-        return p;
-    }, [[]]);
-}
 
 export default class NewsBord extends Component {
 
@@ -29,7 +16,6 @@ export default class NewsBord extends Component {
                 let rowNews = response.data.news;
                 rowNews.map(value => value.dateTime = DateFormat.convertDataTimeToData(value.dateTime))
                 rowNews = sliceArray(rowNews, 3);
-                console.log(rowNews)
                 this.setState({
                     rowNews: rowNews
                 })
@@ -39,7 +25,6 @@ export default class NewsBord extends Component {
 
     render() {
         const rowNews = this.state.rowNews;
-        console.log('rowNews', rowNews)
         return(
             <div className="d-flex justify-content-center">
                 <div className="news-board">
@@ -50,4 +35,15 @@ export default class NewsBord extends Component {
             </div>
         );
     }
+}
+
+function sliceArray(array, size){ // return arrays of roles, arrays length <= size
+    return array.reduce((p,c)=>{
+        if(p[p.length-1].length === size){
+            p.push([]);
+        }
+
+        p[p.length-1].push(c);
+        return p;
+    }, [[]]);
 }
