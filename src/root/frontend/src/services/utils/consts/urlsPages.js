b409617f51fc8be8a3b63e4528a28a3841208318
stormpero <staralex.const@gmail.com>
2021-11-16 17:49:10 +0300

    add consts

---
+++ src/root/frontend/src/services/utils/consts/urlsPages.js
@@ -0,0 +1,6 @@
+
+export const URL_LOGIN = '/login';
+export const URL_REGISTER= '/register';
+export const URL_PROFILE = '/profile';
+export const URL_USER = '/user';
+export const URL_NEWS = '/news';
