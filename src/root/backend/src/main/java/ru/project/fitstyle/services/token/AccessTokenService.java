11040e3094dcef1bcd82cf32fb4517c698128719
FStrean <fustone.steamer@mail.ru>
2021-11-17 19:01:28 +0300

    Changed validateToken method
    feature(AccessTokenService): validateToken method now catches only one exception

--- src/root/backend/src/main/java/ru/project/fitstyle/services/token/AccessTokenService.java
+++ src/root/backend/src/main/java/ru/project/fitstyle/services/token/AccessTokenService.java
@@ -49,15 +49,7 @@ public class AccessTokenService {
                     .setSigningKey(JwtTokenHandler.getSigningKey(accessTokenSecret))
                     .build().parseClaimsJws(token);
             return true;
-        } catch (SignatureException ex) {
-            ex.printStackTrace();
-        } catch (MalformedJwtException ex) {
-            ex.printStackTrace();
-        } catch (ExpiredJwtException ex) {
-            ex.printStackTrace();
-        } catch (UnsupportedJwtException ex) {
-            ex.printStackTrace();
-        } catch (IllegalArgumentException ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return false;
