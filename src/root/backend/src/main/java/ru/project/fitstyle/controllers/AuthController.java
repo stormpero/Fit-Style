b838b4510dc20371fe4520743af294a58cdad78a
FStrean <fustone.steamer@mail.ru>
2021-11-17 20:01:09 +0300

    Changed refresh token method AuthController
    feature: made requestRefreshToken value not hardcoded but taken from application.properties

--- src/root/backend/src/main/java/ru/project/fitstyle/controllers/AuthController.java
+++ src/root/backend/src/main/java/ru/project/fitstyle/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package ru.project.fitstyle.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.GrantedAuthority;
@@ -153,8 +154,9 @@ public class AuthController {
     }
 
     @GetMapping("/refreshtoken")
-    public ResponseEntity<RefreshTokenResponse> refreshToken(@CookieValue(value = "refreshToken", required = false)
-                                                      String requestRefreshToken) {
+    public ResponseEntity<RefreshTokenResponse> refreshToken(@Value("${authentication.auth.authRefreshTokenCookieName}")
+                                                                 @CookieValue(required = false)
+                                                                         String requestRefreshToken) {
         return refreshTokenService
                 .findByToken(requestRefreshToken)
                 .map(refreshTokenService::verifyExpiration)
