e22d279cc0b5aebd40796b8adbf7554e62f90761
stormpero <staralex.const@gmail.com>
2021-11-19 12:41:25 +0300

    add loading news

--- src/root/frontend/src/pages/news/NewsBoard.jsx
+++ src/root/frontend/src/pages/news/NewsBoard.jsx
@@ -1,49 +1,41 @@
-import React, { Component } from "react";
+import React, {useEffect, useState} from "react";
 import NewsRow from "./NewsRow";
 import UserService from "../../services/UserService";
 import DateFormat from "../../services/utils/DateFormat";
+import ArrayHelper from "../../services/utils/ArrayHelper";
 
-export default class NewsBoard extends Component {
+export const NewsBoard = () => {
 
-    state = {
-        rowNews: [],
-    }
-    // news: [{Новость},{Новость},{Новость},{Новость},{Новость},{Новость}]
-    // rowNews: [[{Новость},{Новость},{Новость}],[{Новость},{Новость},{Новость}]]
-    componentDidMount() {
-        UserService.getNews(1).then(
-            response => {
-                let rowNews = response.data.news;
-                rowNews.map(value => value.dateTime = DateFormat.convertDataTimeToData(value.dateTime))
-                rowNews = sliceArray(rowNews, 3);
-                this.setState({
-                    rowNews: rowNews
-                })
-            }
-        );
-    }
+    const [rowNews, setRowNews] = useState([]);
+    const [rowNum, setRowNum] = useState(1);
+    const [hasNews, setHasNews] = useState(true);
 
-    render() {
-        const rowNews = this.state.rowNews;
-        return(
-            <div className="d-flex justify-content-center">
-                <div className="news-board">
-                    {
-                        rowNews.map((param, index) => <NewsRow key={index} news={param}/>)
+    useEffect(() => {
+        const loadNews = () => {
+            UserService.getNews(rowNum).then(
+                response => {
+                    let rowNewsData = response.data.news;
+                    rowNewsData.map(value => value.dateTime = DateFormat.convertDataTimeToData(value.dateTime))
+                    rowNewsData = ArrayHelper.sliceArray(rowNewsData, 3);
+                    setRowNews(prevNews => prevNews.concat(rowNewsData))
+                },
+                error => {
+                    if (error.response.data.statusCode === 400 &&
+                        error.response.data.message === "Failed. Page not found!") {
+                        setHasNews(false);
                     }
-                </div>
+                }
+            )}
+        loadNews();
+    },[rowNum])
+
+    return(
+        <div className="d-flex justify-content-center">
+            <div className="news-board">
+                {rowNews && rowNews.map((param, index) => <NewsRow key={index} news={param}/>)}
+                {hasNews && <button onClick={() => setRowNum(rowNum + 1)}>Ещё</button> }
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-function sliceArray(array, size){ // return arrays of roles, arrays length <= size
-    return array.reduce((p,c)=>{
-        if(p[p.length-1].length === size){
-            p.push([]);
-        }
-
-        p[p.length-1].push(c);
-        return p;
-    }, [[]]);
-}
