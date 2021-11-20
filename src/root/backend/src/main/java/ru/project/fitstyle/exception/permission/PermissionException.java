2819f1ff6792604b679b8def96be6bda6d7b3aaf
FStrean <fustone.steamer@mail.ru>
2021-11-20 18:55:39 +0300

    Updated PermissionController

--- src/root/backend/src/main/java/ru/project/fitstyle/exception/permission/PermissionException.java
+++ src/root/backend/src/main/java/ru/project/fitstyle/exception/permission/PermissionException.java
@@ -1,11 +1,8 @@
 package ru.project.fitstyle.exception.permission;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class PermissionException extends RuntimeException {
-    private static final long serialVersionUID = 31L;
+
+    private static final long serialVersionUID = 1L;
 
     private final int errorCode;
 
