c5cb16a92b3a4d61a99f6569016c98040284d98d
stormpero <staralex.const@gmail.com>
2021-11-21 19:11:39 +0300

    validation start

--- src/root/frontend/src/services/utils/Validation.js
+++ src/root/frontend/src/services/utils/Validation.js
@@ -1,7 +1,11 @@
+import isEmpty from "validator/es/lib/isEmpty";
+import {logDOM} from "@testing-library/react";
+
 class Validation {
     validateNull(obj) {
-        for (let i in obj) {
-            if (obj.hasOwnProperty(i)) {
+        let array = Object.values(obj);
+        for (let i of array) {
+            if (i !== undefined && typeof i === "string" && isEmpty(i)) {
                 return true;
             }
         }
@@ -29,8 +33,39 @@ class Validation {
         let reg = /[A-Za-zA-Яа-яЁё]/;
         return reg.test(String(obj).toLowerCase());
     }
+    //
+    validateSumCount(str, count) {
+        if (str.length > count)
+            return false;
+        return true;
+    }
 
+    validateSingUp(obj) {
+        //true - ошибка
+        // false - Хорошо
+    // Проверка на пустоту
+    // Проверка на корректность
+        /// name surname patronymic
+        if (this.validateNull(obj)) {
+            return { msg: "Заполните поля", result: false }
+        }
 
+        for(let i in obj) {
+            console.log(obj.i)
+            switch (i) {
+                case "email":
+                    if (this.validateEmail(i)) {
+
+                    }
+                break;
+                case "name": {
+                    if (this.validateSumCount(obj.i, 20)) {
+
+                    }
+                }
+            }
+        }
+    }
 
 }
 
