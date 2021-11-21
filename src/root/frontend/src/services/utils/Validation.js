4a135c131bb4c9792addcdb051f896085791fc2d
Igor Temnikov <io.temnikov@bk.ru>
2021-11-21 04:01:34 +0300

    add validating Null

--- src/root/frontend/src/services/utils/Validation.js
+++ src/root/frontend/src/services/utils/Validation.js
@@ -1,3 +1,13 @@
 class Validation {
-    
+    validIsNull(obj) {
+        for (let i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
+
+export default new Validation()
