13fc1fdd16e1636d51a15e1b6e4ba61cce6ea4f6
FStrean <fustone.steamer@mail.ru>
2021-11-22 14:23:27 +0300

    Updated LoginRequest

--- src/root/backend/src/main/java/ru/project/fitstyle/payload/request/auth/LogOutRequest.java
+++
@@ -1,9 +0,0 @@
-package ru.project.fitstyle.payload.request.auth;
-
-public class LogOutRequest {
-    private Long userId;
-
-    public Long getUserId() {
-        return this.userId;
-    }
-}
--- src/root/backend/src/main/java/ru/project/fitstyle/payload/request/auth/LoginRequest.java
+++ src/root/backend/src/main/java/ru/project/fitstyle/payload/request/auth/LoginRequest.java
@@ -1,15 +1,22 @@
 package ru.project.fitstyle.payload.request.auth;
 
+import ru.project.fitstyle.model.subscription.Subscription;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class LoginRequest {
-    @NotBlank(message = "username should not be blank")
-    @Email
+    @NotBlank(message = "email should not be blank")
+    @Size(min=10, max=50, message = "email should be more than 10 and less than 50 characters")
+    @Email(message = "It should be email!")
     private String email;
 
     @NotBlank(message = "password should not be blank")
+    @Size(min=6, max=120, message = "password should be more than 6 and less than 12- characters")
     private String password;
+
+    private Subscription subscription;
 
     public String getEmail() {
         return email;
@@ -25,5 +32,13 @@ public class LoginRequest {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Subscription getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(Subscription subscription) {
+        this.subscription = subscription;
     }
 }
