2519abf5cd6d869bef705cc922dd4bbb122bcd75
1Gvozd1 <62207561+1Gvozd1@users.noreply.github.com>
2021-11-22 14:27:56 +0300

    update news buttons

--- src/root/frontend/src/pages/news/NewsBoard.jsx
+++ src/root/frontend/src/pages/news/NewsBoard.jsx
@@ -6,6 +6,7 @@ import Modal from "../../components/modal/Modal";
 import NewsFormContainer from "./form/NewsFormContainer";
 import NewsService from "../../services/api/NewsService";
 import LStorageUser from "../../services/LStorageUser";
+import "./NewsBoard.css";
 
 export const NewsBoard = () => {
 
@@ -51,14 +52,32 @@ export const NewsBoard = () => {
     return(
         <div className="d-flex justify-content-center">
             <div className="news-board">
-                {isAdmin &&
-                <div>
-                    <button className="btn-primary" onClick={() => setModalActive(true)}>Добавить Новость</button>
-                    <button className={deleteActive ? 'btn-primary' : 'btn-darkgi'} onClick={() => setDeleteActive((prev) => !prev)}>Режим удаления</button>
+
+
+                <div className="d-flex justify-content-around">
+                    {isAdmin &&
+                    <button className={deleteActive ? 'select' : 'noselect'}
+                            onClick={() => setDeleteActive((prev) => !prev)}><span className='text'>Удалить</span><span
+                        className="icon"><svg
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path
+                        d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span>
+                    </button>
+                    }
+                    <h1 className="map-title">Новостная лента</h1>
+                    {isAdmin &&
+                    <button className="add-news" onClick={() => setModalActive(true)}><span
+                        className='text'>Добавить</span><span className="icon"><svg
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" transform="rotate(45)"
+                        viewBox="0 0 24 24"><path
+                        d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span>
+                    </button>
+                    }
                 </div>
-                }
                 {rowNews && rowNews.map((param, index) => <NewsRow key={index} news={param} delete={deleteNews} deleteMode={deleteActive}/>)}
-                {hasNews && <button className="btn-primary" onClick={() => setRowNum(rowNum + 1)}>Ещё</button> }
+                {hasNews && <div className="d-flex justify-content-center"><button className="news-more" onClick={() => setRowNum(rowNum + 1)}><span className="more-news-icon"><svg
+                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" transform="rotate(45)"
+                    viewBox="0 0 24 24"><path
+                    d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span></button></div> }
                 <br/>
                 <br/>
                 <br/>
