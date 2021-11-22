3e61cee495959597393277862e149fb25d325cd3
stormpero <staralex.const@gmail.com>
2021-11-22 18:48:59 +0300

    add calendar localization

--- src/root/frontend/src/pages/calendar/CalendarView.jsx
+++ src/root/frontend/src/pages/calendar/CalendarView.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+import localization from 'moment/locale/ru'
 import "react-big-calendar/lib/css/react-big-calendar.css"
+import {views} from "react-big-calendar/lib/utils/constants";
+import {formats, messagesRu} from "../../services/utils/consts/Calendar";
 
-const localizer = momentLocalizer(moment)
 const myEvents = [
     {
         id: 0,
-        title: 'All Day Event very long title',
-        allDay: true,
-        start: new Date(2015, 3, 0),
-        end: new Date(2015, 3, 1),
+        title: 'Тренировка',
+        start: new Date(2021, 10, 23, 12 - 1, 0, 0),
+        end: new Date(2021, 10, 23, 14, 0, 0),
     },
     {
         id: 1,
-        title: 'Long Event',
-        start: new Date(2015, 3, 7),
-        end: new Date(2015, 3, 10),
+        title: 'Тренировка',
+        start: new Date(2021, 10, 21, 12, 0, 0),
+        end: new Date(2021, 10, 21, 13, 0, 0),
     },
-
     {
         id: 2,
         title: 'DTS STARTS',
-        start: new Date(2022, 1, 13, 0, 0, 0),
-        end: new Date(2022, 1, 14 + 1, 0, 0, 0),
+        start: new Date(2021, 10, 13, 12, 0, 0),
+        end: new Date(2021, 10, 13, 13, 0, 0),
     },
 
     {
@@ -33,15 +33,23 @@ const myEvents = [
         end: new Date(2016, 10, 13, 0, 0, 0),
     },
 ]
+
+const localize = momentLocalizer(moment)
+
 function CalendarView(props) {
+    moment.locale(localization.locale);
+
+
     return (
         <div className="container bg-white" >
             <Calendar
-                localizer={localizer}
+                localizer={localize}
                 events={myEvents}
-                startAccessor="start"
-                endAccessor="end"
-                style={{ height: 800 }}
+                style={{ height: 900 }}
+                views={['month', 'week', 'day']}
+                defaultView={views.WEEK}
+                messages={messagesRu}
+                formats={formats}
             />
         </div>
     );
