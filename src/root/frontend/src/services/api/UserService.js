dc4c76653ff843059d006cbf263e51853075bcad
stormpero <staralex.const@gmail.com>
2021-11-25 15:16:06 +0300

    update paths

--- src/root/frontend/src/services/api/UserService.js
+++ src/root/frontend/src/services/api/UserService.js
@@ -1,18 +1,24 @@
 import api from "./Api";
-import {URL_PROFILE, URL_ROLES, URL_USERTEST} from "../utils/consts/urlsApi";
+import {URL_PROFILE, URL_ROLES, URL_SUBSCRIPTIONTYPE, URL_USERTEST} from "../utils/consts/urlsApi";
 
 class UserService {
-  getUserBoard() {
-    return api.get(URL_USERTEST);
-  }
+    getUserBoard() {
+        return api.get(URL_USERTEST);
+    }
 
-  getProfileInfo(id) {
-    return api.get(URL_PROFILE);
-  }
+    getProfileInfo(id) {
+        return api.get(URL_PROFILE);
+    }
 
-  getRoles() {
-    return api.get(URL_ROLES);
-  }
+    getRoles() {
+        return api.get(URL_ROLES);
+    }
+
+    getSubscriptionType() {
+        return api.get(URL_SUBSCRIPTIONTYPE);
+    }
+
+    get
 }
 
 export default new UserService();
