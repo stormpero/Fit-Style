3e219dd0d845545e8d1b888b0253f90fb7154fd0
FStrean <fustone.steamer@mail.ru>
2021-11-25 17:20:06 +0300

    Reverted TestController

--- src/root/backend/src/main/java/ru/project/fitstyle/controller/TestController.java
+++ src/root/backend/src/main/java/ru/project/fitstyle/controller/TestController.java
@@ -1,20 +1,14 @@
 package ru.project.fitstyle.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import ru.project.fitstyle.exception.refresh.ERefreshTokenError;
-import ru.project.fitstyle.exception.refresh.RefreshTokenException;
-import ru.project.fitstyle.model.user.FitUser;
 import ru.project.fitstyle.repository.UserRepository;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
-    @Autowired
-    UserRepository userRepository;
     @GetMapping("/all")
     public String allAccess() {
         return "Public content.";
@@ -36,17 +30,5 @@ public class TestController {
     @PreAuthorize("hasRole('COACH')")
     public String adminAccess() {
         return "Admin Board.";
-    }
-
-    @GetMapping("/exception_check")
-    public void exceptionCheck() {
-        throw new RefreshTokenException("f3tdrgsdfgsd", ERefreshTokenError.NOT_FOUND);
-    }
-
-    @GetMapping("/get_dicks")
-    public ResponseEntity<?> getDO() {
-        long id = 1;
-        FitUser user = userRepository.getById(id);
-        return ResponseEntity.ok(user);
     }
 }
