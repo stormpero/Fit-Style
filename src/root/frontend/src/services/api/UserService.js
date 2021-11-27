713c0cbde52e02f9a386a04170b72394c46e4676
stormpero <staralex.const@gmail.com>
2021-11-27 15:10:32 +0300

    update api urls

--- src/root/frontend/src/services/api/UserService.js
+++ src/root/frontend/src/services/api/UserService.js
@@ -1,13 +1,24 @@
 import api from "./Api";
-import {URL_PROFILE, URL_ROLES, URL_SUBSCRIPTIONTYPE, URL_TRAINING, URL_USERTEST} from "../utils/consts/urlsApi";
+import {
+    URL_PROFILE_IMG,
+    URL_PROFILE_INFO,
+    URL_ROLES,
+    URL_SUBSCRIPTIONTYPE,
+    URL_TRAINING,
+    URL_USERTEST
+} from "../utils/consts/urlsApi";
 
 class UserService {
     getUserBoard() {
         return api.get(URL_USERTEST);
     }
 
-    getProfileInfo(id) {
-        return api.get(URL_PROFILE);
+    getProfileInfo() {
+        return api.get(URL_PROFILE_INFO);
+    }
+
+    getProfileImg() {
+        return api.get(URL_PROFILE_IMG, {responseType: 'blob'})
     }
 
     getRoles() {
