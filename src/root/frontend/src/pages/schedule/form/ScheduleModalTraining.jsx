c2a0bd8eb983865e5e14efae190a3af448d1597a
1Gvozd1 <62207561+1Gvozd1@users.noreply.github.com>
2021-11-29 18:06:46 +0300

    Add information about workout

--- src/root/frontend/src/pages/schedule/form/ScheduleModalTraining.jsx
+++ src/root/frontend/src/pages/schedule/form/ScheduleModalTraining.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 function ScheduleModalTraining({setModalActive, event, deleteTraining}) {
     return (
         <div>
-            <button onClick={() => deleteTraining(event)}>Удалить</button>
+            <h4 className="title">Информация о тренировке</h4>
+            <div className="d-flex justify-content-around">
+                <p>
+                    <label> Время </label>
+                    <strong>20.11.2021</strong>
+                </p>
+                <p>
+                    <label> ФИО тренера </label>
+                    <strong>20.11.2021</strong>
+                </p>
+                <p>
+                    <label> Статус </label>
+                    <strong>20.11.2021</strong>
+                </p>
+            </div>
+            <center>
+                    <button className="btn btn-danger mt-5" onClick={() => deleteTraining(event)}>Удалить</button>
+            </center>
         </div>
     );
 }
