4b45d8c52b38ee79940cab0f71bbdc5409a4016a
1Gvozd1 <62207561+1Gvozd1@users.noreply.github.com>
2021-12-02 01:37:08 +0300

    Map analysis

--- src/root/frontend/src/pages/map/Map.jsx
+++ src/root/frontend/src/pages/map/Map.jsx
@@ -14,8 +14,9 @@ export const Map = () => {
             <div className="map-picture-container">
                 <center>
                 <div className="grid-wrapper">
-                    <div className="reception right bottom">
+                    <div className='reception'>
                         <img className="map-block-logo" src={reception} alt="Fit-Style"/>
+
                         <div className="map-content">
                             <h4>Reception</h4>
                             <span>Время работы</span>
@@ -23,34 +24,7 @@ export const Map = () => {
                             <span>Пн-Вс: 6:00-23:00</span>
                         </div>
                     </div>
-                    <div className="right bottom left">
-                        <img className="map-block-log" src={dumbbell} alt="Fit-Style"/>
-                        <div className="map-content">
-                            <h4>Тренажерный зал</h4>
-                            <span>Время работы</span>
-                            <br/>
-                            <span>Пн-Вс: 10:00-22:00</span>
-                        </div>
-                    </div>
-                    <div className="tall left bottom">
-                        <img className="map-block-logo" src={group} alt="Fit-Style"/>
-                        <div className="map-content">
-                            <h4>Зал групповых занятий</h4>
-                            <span>Время работы</span>
-                            <br/>
-                            <span>Пн-Вс: 8:00-23:00</span>
-                        </div>
-                    </div>
-                    <div className="wide top right bottom">
-                        <img className="map-block-logo" src={boxing} alt="Fit-Style"/>
-                        <div className="map-content">
-                            <h1>Зал бокса</h1>
-                            <span>Время работы</span>
-                            <br/>
-                            <span>Пн, Ср, Пт: 10:00-21:00</span>
-                        </div>
-                    </div>
-                    <div className="top right bottom">
+                    <div>
                         <img className="map-block-logo" src={dumbbell} alt="Fit-Style"/>
                         <div className="map-content">
                             <h4>Тренажерный зал</h4>
@@ -59,7 +33,34 @@ export const Map = () => {
                             <span>Пн-Вс: 10:00-22:00</span>
                         </div>
                     </div>
-                    <div className="big top left">
+                    <div className="tall">
+                        <img className="map-block-logo" src={group} alt="Fit-Style"/>
+                        <div className="map-content">
+                            <h4>Зал групповых занятий</h4>
+                            <span>Время работы</span>
+                            <br/>
+                            <span>Пн-Вс: 8:00-23:00</span>
+                        </div>
+                    </div>
+                    <div className='wide' >
+                        <img className="map-block-logo" src={boxing} alt="Fit-Style"/>
+                        <div className="map-content">
+                            <h1>Зал бокса</h1>
+                            <span>Время работы</span>
+                            <br/>
+                            <span>Пн, Ср, Пт: 10:00-21:00</span>
+                        </div>
+                    </div>
+                    <div>
+                        <img className="map-block-logo" src={dumbbell} alt="Fit-Style"/>
+                        <div className="map-content">
+                            <h4>Тренажерный зал</h4>
+                            <span>Время работы</span>
+                            <br/>
+                            <span>Пн-Вс: 10:00-22:00</span>
+                        </div>
+                    </div>
+                    <div className='big ' >
                         <img className="map-block-logo" src={pool} alt="Fit-Style"/>
                         <div className="map-content">
                             <h1>Бассейн</h1>
@@ -68,7 +69,7 @@ export const Map = () => {
                             <span>Пн-Сб: 10:00-20:00</span>
                         </div>
                     </div>
-                    <div className="top right">
+                    <div>
                         <img className="map-block-logo" src={dumbbell} alt="Fit-Style"/>
                         <div className="map-content">
                             <h4>Тренажерный зал</h4>
