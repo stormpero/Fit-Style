11f6b6765cf1a2836c7a9f3672033212fdcebc19
stormpero <staralex.const@gmail.com>
2021-12-03 17:10:41 +0300

    Update style & fix news bug

--- src/fit-style-frontend/src/pages/news/NewsRow.jsx
+++ src/fit-style-frontend/src/pages/news/NewsRow.jsx
@@ -5,7 +5,7 @@ import "./News.css";
 const NewsRow = (props) => {
     return(
         <div>
-            <div className="row-news d-flex justify-content-between">
+            <div className="row-news d-flex justify-content-start">
                 {props.news.map((param, index) => <News key={index} content={param} delete={props.delete} deleteMode={props.deleteMode}/>)}
             </div>
         </div>
