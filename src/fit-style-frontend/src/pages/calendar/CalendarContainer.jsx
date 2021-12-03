4a195aeb9b286d29805388ab58432324b5719fae
stormpero <staralex.const@gmail.com>
2021-12-03 16:18:04 +0300

    refactor

--- src/fit-style-frontend/src/pages/calendar/CalendarContainer.jsx
+++ src/fit-style-frontend/src/pages/calendar/CalendarContainer.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {CalendarView} from "./CalendarView";
 import ToastMessages from "../../components/toastmessages/ToastMessages";
 import ScheduleApi from "../../services/api/schedule/ScheduleApi";
+import TrainingService from "../../services/training/TrainingService";
 
 export const CalendarContainer = () => {
     const [trainingsList, setTrainingsList] = useState([]);
@@ -9,22 +10,7 @@ export const CalendarContainer = () => {
     useEffect(() => {
         ScheduleApi.getTrainings().then(
             response => {
-                let {personalTrainings, groupTrainings} = response.data;
-                personalTrainings.forEach(value => {
-                    value.startDate = new Date(value.startDate);
-                    value.endDate = new Date(value.endDate);
-                    value.isPersonal = true;
-                    return value;
-                })
-
-                groupTrainings.forEach(value => {
-                    value.startDate = new Date(value.startDate);
-                    value.endDate = new Date(value.endDate);
-                    return value;
-                })
-
-                let trainingsListTemp = personalTrainings.concat(groupTrainings);
-                console.log(trainingsListTemp)
+                let trainingsListTemp = TrainingService.concatTrainings(response.data);
                 setTrainingsList(trainingsListTemp);
             },
             error => {
