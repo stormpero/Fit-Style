7d518aa3ff3e45c6d6e5861ed799dd813d5ed02f
FStrean <fustone.steamer@mail.ru>
2021-12-03 20:47:07 +0300

    Alexander said to make refreshCookie expire

--- src/fit-style-backend/src/main/java/ru/project/fitstyle/service/impl/RefreshTokenCookieService.java
+++ src/fit-style-backend/src/main/java/ru/project/fitstyle/service/impl/RefreshTokenCookieService.java
@@ -20,7 +20,6 @@ public class RefreshTokenCookieService implements CookieService {
     @Override
     public HttpCookie createCookie(String token, Long duration) {
         return ResponseCookie.from(cookieName, token)
-                .maxAge(duration/1000)
                 .httpOnly(true)
                 .secure(true)
                 .path("/")
