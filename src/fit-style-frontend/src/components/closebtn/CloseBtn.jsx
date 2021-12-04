fcb80871bf01c78b7013c8faf42e301c37d2d61e
stormpero <staralex.const@gmail.com>
2021-12-05 00:04:23 +0300

    add component for close button

--- src/fit-style-frontend/src/components/closebtn/CloseBtn.jsx
+++ src/fit-style-frontend/src/components/closebtn/CloseBtn.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import "./CloseBtn.css"
 
-export const CloseBtn = () => {
+export const CloseBtn = ({onClickEvent}) => {
     return (
         <div>
-
+            <button
+                type="button"
+                className="btn-close-modal"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={onClickEvent}
+            >
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
     );
 };
