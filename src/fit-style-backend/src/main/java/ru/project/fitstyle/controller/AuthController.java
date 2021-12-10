bf5ce555aecb7b2c207003777da801698f7ed359
FStrean <fustone.steamer@mail.ru>
2021-12-10 18:36:08 +0300

    Moved adding user to UsersController
    as long as it is better fits to be placed there

--- src/fit-style-backend/src/main/java/ru/project/fitstyle/controller/AuthController.java
+++ src/fit-style-backend/src/main/java/ru/project/fitstyle/controller/AuthController.java
@@ -39,13 +39,7 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
 
-    private final PasswordEncoder encoder;
-
     private final UserService userService;
-
-    private final RoleService roleService;
-
-    private final SubscriptionTypeService subscriptionTypeService;
 
     private final TokenService accessTokenService;
 
@@ -55,31 +49,22 @@ public class AuthController {
 
     private final AuthService authService;
 
-    private final StorageService imageStorageService;
-
     @Autowired
-    public AuthController(final AuthenticationManager authenticationManager,final PasswordEncoder encoder,
+    public AuthController(final AuthenticationManager authenticationManager,
                           final UserService userService,
-                          final RoleService roleService,
-                          final SubscriptionTypeService subscriptionTypeService,
                           @Qualifier("accessTokenService") final TokenService accessTokenService,
                           @Qualifier("refreshTokenService") final TokenService refreshTokenService,
                           final CookieService refreshTokenCookieService,
-                          final AuthService authService,
-                          final StorageService imageStorageService) {
+                          final AuthService authService) {
         this.authenticationManager = authenticationManager;
         this.userService = userService;
-        this.roleService = roleService;
-        this.subscriptionTypeService = subscriptionTypeService;
-        this.encoder = encoder;
         this.accessTokenService = accessTokenService;
         this.refreshTokenService = refreshTokenService;
         this.refreshTokenCookieService = refreshTokenCookieService;
         this.authService = authService;
-        this.imageStorageService = imageStorageService;
     }
 
-    @PostMapping("/signin")
+    @PostMapping("/sign-in")
     public ResponseEntity<LoginResponse> authenticateUser(@Valid @RequestBody final LoginRequest loginRequest) {
         final Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(
@@ -102,25 +87,7 @@ public class AuthController {
                         userDetails.getUsername(), accessToken, roles));
     }
 
-    @PostMapping("/signup")
-    @PreAuthorize("hasRole('MODERATOR')")
-    public ResponseEntity<SuccessMessage> registerUser(@Valid @RequestPart(value = "request") final SignupRequest request,
-                                                       @RequestPart(value = "image", required = false) final MultipartFile image) {
-        userService.validateEmail(request.getEmail());
-
-        FitUser fitUser = createFitUser(request);
-
-        fitUser.setImgURL(imageStorageService.store(image));
-
-        userService.saveUser(fitUser, roleService.createRoles(request.getRoles()),
-                subscriptionTypeService.createFitUserSubscription(request.getSubscriptionTypeInfo().getSubscriptionTypeId(),
-                        request.getSubscriptionTypeInfo().getContractNumber()));
-
-        return ResponseEntity.ok(
-                new SuccessMessage("User registered successfully!"));
-    }
-
-    @GetMapping("/refreshtoken")
+    @GetMapping("/refresh-token")
     public synchronized ResponseEntity<RefreshTokenResponse> refreshToken(@CookieValue(value="refreshToken", required = false)
                                                       final String requestRefreshToken) {
         final FitUser fitUser = ((RefreshToken) (refreshTokenService.validate(requestRefreshToken))).getUser();
@@ -146,14 +113,5 @@ public class AuthController {
         httpHeaders.add(HttpHeaders.SET_COOKIE, refreshTokenCookieService.createCookie(refreshToken,
                 refreshTokenService.getExpirationMs()).toString());
         return httpHeaders;
-    }
-
-    private FitUser createFitUser(final SignupRequest request) {
-        return new FitUser(request.getName(), request.getSurname(),
-                request.getPatronymic(), request.getEmail(),
-                encoder.encode(request.getPassword()),
-                request.getAge(), request.getGender(),
-                request.getBirthdate(), request.getTelephone(),
-                request.getPassport(), request.getAddress());
     }
 }
--- src/fit-style-backend/src/main/java/ru/project/fitstyle/controller/UsersController.java
+++ src/fit-style-backend/src/main/java/ru/project/fitstyle/controller/UsersController.java
@@ -3,24 +3,42 @@ package ru.project.fitstyle.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import ru.project.fitstyle.controller.response.coach.AllCoachesResponse;
+import org.springframework.web.multipart.MultipartFile;
+import ru.project.fitstyle.controller.request.auth.SignupRequest;
+import ru.project.fitstyle.controller.response.SuccessMessage;
 import ru.project.fitstyle.controller.response.fitusers.AllFitUserResponse;
 import ru.project.fitstyle.model.entity.user.FitUser;
-import ru.project.fitstyle.service.UserService;
+import ru.project.fitstyle.service.*;
 
-import java.util.List;
+import javax.validation.Valid;
 
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
+    private final PasswordEncoder encoder;
 
     private final UserService userService;
 
+    private final RoleService roleService;
+
+    private final SubscriptionTypeService subscriptionTypeService;
+
+    private final StorageService imageStorageService;
+
     @Autowired
-    public UsersController(final UserService userService) {
+    public UsersController(final PasswordEncoder encoder,
+                           final UserService userService,
+                           final RoleService roleService,
+                           final SubscriptionTypeService subscriptionTypeService,
+                           final StorageService imageStorageService) {
+        this.encoder = encoder;
         this.userService = userService;
+        this.roleService = roleService;
+        this.subscriptionTypeService = subscriptionTypeService;
+        this.imageStorageService = imageStorageService;
     }
 
     @GetMapping("/all")
@@ -29,5 +47,32 @@ public class UsersController {
         return ResponseEntity.ok(
                 new AllFitUserResponse(userService.getAllUsers())
         );
+    }
+
+    @PostMapping("/add")
+    @PreAuthorize("hasRole('MODERATOR')")
+    public ResponseEntity<SuccessMessage> add(@Valid @RequestPart(value = "request") final SignupRequest request,
+                                                       @RequestPart(value = "image", required = false) final MultipartFile image) {
+        userService.validateEmail(request.getEmail());
+
+        FitUser fitUser = createFitUser(request);
+
+        fitUser.setImgURL(imageStorageService.store(image));
+
+        userService.saveUser(fitUser, roleService.createRoles(request.getRoles()),
+                subscriptionTypeService.createFitUserSubscription(request.getSubscriptionTypeInfo().getSubscriptionTypeId(),
+                        request.getSubscriptionTypeInfo().getContractNumber()));
+
+        return ResponseEntity.ok(
+                new SuccessMessage("User registered successfully!"));
+    }
+
+    private FitUser createFitUser(final SignupRequest request) {
+        return new FitUser(request.getName(), request.getSurname(),
+                request.getPatronymic(), request.getEmail(),
+                encoder.encode(request.getPassword()),
+                request.getAge(), request.getGender(),
+                request.getBirthdate(), request.getTelephone(),
+                request.getPassport(), request.getAddress());
     }
 }
