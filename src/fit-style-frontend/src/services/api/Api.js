f84d2c1fb36bd6f7adc0de214f7a1740cb46cd26
stormpero <staralex.const@gmail.com>
2021-12-10 16:04:24 +0300

    update refreshtoken, added queue of functions

--- src/fit-style-frontend/src/services/api/Api.js
+++ src/fit-style-frontend/src/services/api/Api.js
@@ -11,50 +11,73 @@ instance.interceptors.request.use(
     (config) => {
         const accessToken = JwtService.getAccessToken();
         if (accessToken) {
-            config.headers["Authorization"] = 'Bearer ' + accessToken;
+            config.headers.Authorization = 'Bearer ' + accessToken;
         }
         return config;
     },
-    error => {
-        return Promise.reject(error);
-    }
+    error => Promise.reject(error)
 );
 
 let isAlreadyFetchingAccessToken = false;
+let subscribers = [];
 
 instance.interceptors.response.use(
     (response) => {
         return response;
     },
-    async error => {
-        const config = error.config;
-        if (config.url !== URL_AUTH && error?.response?.status === 401 && !config.retry) {
-            config.retry = true;
-
-            try {
-                if (!isAlreadyFetchingAccessToken) {
-                    isAlreadyFetchingAccessToken = true;
-                    let response = await instance.get(URL_REFRESHTOKEN, {
-                        withCredentials: true
-                    })
-                    const {accessToken} = response.data;
-                    JwtService.updateAccessToken(accessToken);
-                    isAlreadyFetchingAccessToken = false;
-                }
-                return instance(config);
-            } catch (_error) {
-                if (_error.response.data.errorCode === 2) {
-                    LStorageUser.remove();
-                    window.location.reload();
-                }
-                return Promise.reject(_error);
-            }
-        } else {
-            return Promise.reject(error);
+    error => {
+        if (isTokenExpiredError(error)) {
+            return resetTokenAndReattemptRequest(error)
         }
+        return Promise.reject(error)
     }
 );
 
-export default instance;
+function isTokenExpiredError(error) {
+    const {config, response} = error;
+    return config?.url !== URL_AUTH && response?.status === 401
+}
 
+async function resetTokenAndReattemptRequest(error) {
+    try {
+        const {response: errorResponse} = error;
+
+        const retryOriginalRequest = new Promise(resolve => {
+            addSubscriber(access_token => {
+                errorResponse.config.headers.Authorization = 'Bearer ' + access_token;
+                resolve(axios(errorResponse.config));
+            });
+        });
+
+        if (!isAlreadyFetchingAccessToken) {
+            isAlreadyFetchingAccessToken = true;
+            let response = await instance.get(URL_REFRESHTOKEN, {
+                withCredentials: true
+            })
+            const {accessToken} = response.data;
+            JwtService.updateAccessToken(accessToken);
+            isAlreadyFetchingAccessToken = false;
+            onAccessTokenFetched(accessToken);
+        }
+        return retryOriginalRequest;
+    } catch (err) {
+        console.log(err.response)
+        if (err.response.data.errorCode === 2) {
+            LStorageUser.remove();
+            window.location.reload();
+        }
+        return Promise.reject(err);
+    }
+}
+
+function onAccessTokenFetched(accessToken) {
+    subscribers.forEach(callback => callback(accessToken));
+    subscribers = [];
+}
+
+function addSubscriber(callback) {
+    subscribers.push(callback);
+}
+
+export default instance;
 
