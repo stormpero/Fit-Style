b548cc16fb9d3c802553f3ce813390d62a0333d3
FStrean <fustone.steamer@mail.ru>
2021-12-12 17:51:11 +0300

    Updated script

--- scripts/set-up-secure/setup.bat
+++ scripts/set-up-secure/setup.bat
@@ -1,16 +1,16 @@
 @echo off
-Powershell.exe  -executionpolicy Bypass -File download-chocolate.ps1
+powershell -executionpolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 choco install mkcert
-cd ../../src/fit-style-frontend
+cd ..\..\src\fit-style-frontend
 del .env
 mkcert -install
-mkcert -cert-file C:/Users/%USERNAME%/cert.pem -key-file C:/Users/%USERNAME%/key.pem localhost
+mkcert -cert-file %USERPROFILE%\cert.pem -key-file %USERPROFILE%\key.pem localhost
 
 @echo HTTPS=true >> .env
-@echo SSL_CRT_FILE=C:/Users/%USERNAME%/cert.pem >> .env
-@echo SSL_KEY_FILE=C:/Users/%USERNAME%/key.pem >> .env
+@echo SSL_CRT_FILE=%USERPROFILE%\cert.pem >> .env
+@echo SSL_KEY_FILE=%USERPROFILE%\key.pem >> .env
 
-cd ../fit-style-backend/src/main/resources/secure
+cd ..\fit-style-backend\src\main\resources\secure
 	
 mkcert -pkcs12 localhost
 pause
