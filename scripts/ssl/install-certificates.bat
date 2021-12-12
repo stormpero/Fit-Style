a876cee0cbe5b2bc67808e91a4cd5de70ffc58c4
FStrean <fustone.steamer@mail.ru>
2021-12-12 20:52:24 +0300

    Fixed a new bat error

--- scripts/ssl/install-certificates.bat
+++ scripts/ssl/install-certificates.bat
@@ -1,8 +1,5 @@
 @echo off
 
-powershell -executionpolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
-choco install mkcert
-
 cd ..\..\fit-style-backend\src\main\resources\secure
 mkcert -pkcs12 localhost
 
