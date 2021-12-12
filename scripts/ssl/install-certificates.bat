38c32feff2d4ca4b07890af84e4be8c85e9f04f1
FStrean <fustone.steamer@mail.ru>
2021-12-12 22:22:44 +0300

    i dunno

--- scripts/ssl/install-certificates.bat
+++ scripts/ssl/install-certificates.bat
@@ -1,9 +1,10 @@
 @echo off
 
+mkcert -install
+
 cd ..\..\src\fit-style-backend\src\main\resources\secure
 mkcert -pkcs12 localhost
 
-mkcert -install
 mkcert -cert-file %USERPROFILE%\cert.pem -key-file %USERPROFILE%\key.pem localhost
 
 pause
